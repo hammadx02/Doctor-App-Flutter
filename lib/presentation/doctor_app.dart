76cf9a87266d8eb6f709c67462992196b00aa343
hammadx02 <hammadaali02@gmail.com>
2024-01-26 18:34:58 +0500

    updated

--- lib/presentation/doctor_app.dart
+++ lib/presentation/doctor_app.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:doctor_app/constants.dart';
 import 'package:doctor_app/presentation/banner.dart';
